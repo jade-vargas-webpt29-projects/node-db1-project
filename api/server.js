739d83fc084119e943d08e3e8657b169dc1a0196
jadevargas92 <jadevargas92@gmail.com>
2021-09-15 22:11:03 -0500

    Did SQL Queries and Created Router Model and Middleware

--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
-const express = require("express");
+const express = require('express');
+const accountsRouter = require('./accounts/accounts-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter);
+
+server.use('*', (req, res, next) => {
+  next({ status: 404, message: 'not found' });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: `Uh oh!: ${err.message}` });
+});
 
 module.exports = server;
